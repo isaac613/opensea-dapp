8854edd4e29fb5df2cdb2eca75fbf89751b300ec
Isaac <isaacseligcoding@gmail.com>
2022-06-01 17:23:10 +0100

    init connect

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import "./App.css";
 import { ethers } from "ethers";
-
-const getData = () => {
-  const options = { method: "GET", headers: { Accept: "application/json" } };
-
-  fetch("https://api.opensea.io/api/v1/collections?offset=0&limit=50", options)
-    .then((response) => response.json())
-    .then((response) => console.log(response))
-    .catch((err) => console.error(err));
-};
+import { useState } from "react";
 
 function App() {
+  const [account, setAccount] = useState("");
+
+  const connect = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    let res = await provider.send("eth_requestAccounts", []);
+    console.log(res);
+  };
+
+  const getData = () => {
+    const options = { method: "GET", headers: { Accept: "application/json" } };
+
+    fetch(
+      "https://api.opensea.io/api/v1/collections?offset=0&limit=50",
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => console.log(response))
+      .catch((err) => console.error(err));
+  };
   return (
     <div className="App">
+      <button onClick={connect}>connect</button>
       <button onClick={getData}>Get Data</button>
     </div>
   );
