8c77823d555ba94c6ce19dcafdfdcfe32e05eaa1
Isaac <isaacseligcoding@gmail.com>
2022-06-01 17:41:09 +0100

    data retieve and connect complete

--- src/App.js
+++ src/App.js
@@ -4,26 +4,33 @@ import { useState } from "react";
 
 function App() {
   const [account, setAccount] = useState("");
+  const [data, setData] = useState([]);
 
   const connect = async () => {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     let res = await provider.send("eth_requestAccounts", []);
     setAccount(res[0]);
+    getData(res[0]);
   };
 
+  console.log(data);
   const getData = (_account) => {
     const options = { method: "GET", headers: { Accept: "application/json" } };
 
     fetch(
-      `https://api.opensea.io/api/v1/collections?asset_owner=${_account}offset=0&limit=50`,
+      `https://api.opensea.io/api/v1/assets?owner=${_account}&order_direction=desc&limit=20&include_orders=false`,
       options
     )
       .then((response) => response.json())
-      .then((response) => console.log(response))
+      .then((response) => {
+        setData(response.assets);
+        console.log(response);
+      })
       .catch((err) => console.error(err));
   };
   return (
     <div className="App">
+      <p>{account}</p>
       <button onClick={connect}>connect</button>
       <button onClick={getData}>Get Data</button>
     </div>
